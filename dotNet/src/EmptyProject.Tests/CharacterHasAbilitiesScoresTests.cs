ce9549b2e0bbff8ec1f0da3ea5fd73d51ccb2c03
Clayton Hunt <clayton.hunt@digitalcomply.com>
2014-06-19 17:47:13 -0400

    Added feature test for Ability Scores

---
+++ dotNet/src/EmptyProject.Tests/CharacterHasAbilitiesScoresTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmptyProject.Tests
+{
+    [TestClass]
+    public class CharacterHasAbilitiesScoresTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
